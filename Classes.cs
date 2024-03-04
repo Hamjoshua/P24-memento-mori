c720fe2c7636e62c32883a029818d9d7ca5ca292
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2024-03-04 15:50:11 +0700

    first commit

---
+++ Classes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace app
+{
+    [Serializable]
+    public class TextFile
+    {
+        public string FileName;
+        public string Extension; // нужно ли?
+        public string Data;
+
+        public void SerializeToBinary()
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (Stream fileStream = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binaryFormatter.Serialize(fileStream, this);
+            }
+        }
+
+        static public TextFile DeserealizeFromBinary(string filePath)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            TextFile textFile;
+
+            using (Stream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                textFile = (TextFile) binaryFormatter.Deserialize(fileStream);
+            }
+
+            return textFile;
+        }
+    }
+}
